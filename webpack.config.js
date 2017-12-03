2d5f1818e9e5829ef76cf75c7ed0d0aff82991ca
xuqichuang <xqc1319681561@163.com>
2017-12-03 18:13:50 +0800

    完成版个人网站

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,74 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: {
         index: './src/app.js',
         home: './src/home.js'
     },
-    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'), //写成绝对路径的形式 resove 相当于 join ,压缩在dist文件夹下 
+        path: path.resolve(__dirname, 'dist'), //写成绝对路径的形式 resolve 相当于 join ,压缩在dist文件夹下 
         publicPath: 'dist/' //打包之后静态资源文件的前缀路径
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    },
     module: {
+
         rules: [{
-                test: /\.css$/, //css加载器
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/, //图片加载器
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
+            test: /\.js$/, //js加载器
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
             }
-        ]
+        }, {
+            test: /\.css$/, //css加载器
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/, //图片加载器
+            use: [
+                'file-loader'
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader'
+            ]
+        }, {
+            test: /\.scss$/, //sass加载器
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
 
     }
 };
+
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = [
+        new webpack.DefinePlugin({
+            'progress.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
